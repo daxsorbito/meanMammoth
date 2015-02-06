56432aa1504f86b7f2120902929d8cdde9d4579c
daxsorbito <daxsorbito@gmail.com>
2015-02-06 23:05:04 +0800

    Fix the menu issue added a sample Items menu

--- public/modules/items/config/items.client.config.js
+++ public/modules/items/config/items.client.config.js
@@ -4,8 +4,8 @@
 angular.module('items').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('Items', 'Items', 'items', 'dropdown', '/items(/create)?');
-		Menus.addSubMenuItem('Items', 'items', 'List Items', 'items');
-		Menus.addSubMenuItem('Items', 'items', 'New Item', 'items/create');
+		Menus.addMenuItem('topbar', 'Items', 'items', 'dropdown', '/items(/create)?');
+		Menus.addSubMenuItem('topbar', 'items', 'List Items', 'items');
+		Menus.addSubMenuItem('topbar', 'items', 'New Item', 'items/create');
 	}
 ]);
